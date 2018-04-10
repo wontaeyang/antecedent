0ba4abbfd9eb251522663787bc1dbe4879fb2e4b
Wontae Yang <wontaeyang@gmail.com>
2018-04-10 15:56:02 -0400

    adjust to allow active record version 5

--- lib/antecedent.rb
+++ lib/antecedent.rb
@@ -1,8 +1,8 @@
 require "antecedent/version"
 require "active_record"
 
-if ActiveRecord::VERSION::STRING =~ /^5\.2/
-  require "antecedent/active_record_5_2"
+if ActiveRecord::VERSION::STRING =~ /^5\./
+  require "antecedent/active_record_5"
 else
   require "antecednt/fallback"
 end
